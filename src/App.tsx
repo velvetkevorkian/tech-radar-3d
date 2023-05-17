4a4e2ecad6998048864cf8faff7a474c5e9dd9e7
Kyle Macquarrie <kyle_macquarrie@next.co.uk>
2023-05-17 13:25:58 +0100

    Add Leva GUI

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,22 @@ import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { buildRings } from './utils/buildRings'
 import { Ring } from './components/Ring'
+import { useControls } from 'leva'
 
 const rings = buildRings(items)
 
 function App() {
+  const debug = useControls('debug', { enabled: true })
+  const performance = useControls('performance', { enabled: true })
+
   return (
     <Canvas>
-      <Perf position="top-left" />
+      {performance.enabled && <Perf position="top-left" />}
+
       <OrbitControls makeDefault />
-      <axesHelper args={[5]} />
+
+      {debug.enabled && <axesHelper args={[5]} />}
+
       {rings.map((r, index) => (
         <Ring ring={r} ringSize={index} key={r.name} />
       ))}

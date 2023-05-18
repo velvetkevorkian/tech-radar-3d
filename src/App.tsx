688b873834c02060e73e6c26d67398d11c4e31cc
Kyle Macquarrie <kyle_macquarrie@next.co.uk>
2023-05-18 10:53:37 +0100

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,17 @@ import { Perf } from 'r3f-perf'
 import { buildRings } from './utils/buildRings'
 import { Ring } from './components/Ring'
 import { useControls } from 'leva'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { quadrants } from './types'
 
 const rings = buildRings(items)
 
-function App() {
+function ThreeApp() {
   const debug = useControls('debug', { enabled: true })
   const performance = useControls('performance', { enabled: true })
-
   return (
     <Canvas>
-      {performance.enabled && <Perf position="top-left" />}
+      {performance.enabled && <Perf position="bottom-right" />}
 
       <OrbitControls makeDefault />
 
@@ -24,6 +25,24 @@ function App() {
         <Ring ring={r} ringSize={index} key={r.name} />
       ))}
     </Canvas>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">All</Link>
+        {quadrants.map((q) => (
+          <Link to={q} key={q}>
+            {q.replaceAll('-', ' ')}
+          </Link>
+        ))}
+      </nav>
+      <Routes>
+        <Route path="/:quadrant?" element={<ThreeApp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/Ring.tsx
+++ src/components/Ring.tsx
@@ -2,8 +2,11 @@ import { Text } from '@react-three/drei'
 import type { RingProps } from '../types'
 import { ringMaterial } from '../utils/materials'
 import { RadarItem } from './RadarItem'
+import { useParams } from 'react-router-dom'
 
 export function Ring({ ring, ringSize }: RingProps) {
+  const { quadrant } = useParams()
+  console.log({ quadrant })
   return (
     <mesh material={ringMaterial}>
       <sphereGeometry args={[ringSize + 4]} />
@@ -11,11 +14,11 @@ export function Ring({ ring, ringSize }: RingProps) {
         {ring.name}
       </Text>
 
-      {ring.quadrants.map((quad) => {
-        return quad.items.map(({ item, position }) => (
+      {ring.quadrants.map((quad) =>
+        quad.items.map(({ item, position }) => (
           <RadarItem key={item.id} item={item} position={position} />
         ))
-      })}
+      )}
     </mesh>
   )
 }

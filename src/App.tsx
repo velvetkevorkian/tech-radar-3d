efbe1041bd5d6eef6edddfcc4919160b67ecbccc
Kyle Macquarrie <kyle_macquarrie@next.co.uk>
2023-05-22 10:07:21 +0100

    Extract ThreeApp to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,6 @@
-import { Canvas } from '@react-three/fiber'
-import { items } from './utils/parse-csv'
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
-import { buildRings } from './utils/buildRings'
-import { Ring } from './components/Ring'
-import { useControls } from 'leva'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import { quadrants } from './types'
-import { SelectedProvider } from './SelectedContext'
-
-const rings = buildRings(items)
-
-function ThreeApp() {
-  const debug = useControls('debug', { enabled: true })
-  const performance = useControls('performance', { enabled: true })
-
-  return (
-    <Canvas>
-      {performance.enabled && <Perf position="bottom-right" />}
-
-      <OrbitControls makeDefault />
-
-      {debug.enabled && <axesHelper args={[5]} />}
-
-      <SelectedProvider>
-        {rings.map((r, index) => (
-          <Ring ring={r} ringSize={index} key={r.name} />
-        ))}
-      </SelectedProvider>
-    </Canvas>
-  )
-}
+import { ThreeApp } from './components/ThreeApp'
 
 function App() {
   return (

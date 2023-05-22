f8158d1488bb7c679b42d9a5f4be6b8d124e98da
Kyle Macquarrie <kyle_macquarrie@next.co.uk>
2023-05-22 17:07:38 +0100

    Remove controls and fix camera position

--- src/components/ThreeApp.tsx
+++ src/components/ThreeApp.tsx
@@ -1,27 +1,18 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
-import { useControls } from 'leva'
 import { SelectedProvider } from '../SelectedContext'
 import { Nav } from './Nav'
 import { Radar } from './Radar'
 
 export function ThreeApp() {
-  const debug = useControls('debug', { enabled: true })
-  const performance = useControls('performance', { enabled: true })
-
   return (
     <>
       <Nav />
 
-      <Canvas>
-        {performance.enabled && <Perf position="bottom-right" />}
-
+      <Canvas camera={{ position: [0, 0, 25] }}>
         <OrbitControls makeDefault />
 
-        {debug.enabled && <axesHelper args={[5]} />}
-
-        {/* <ambientLight /> */}
+        <axesHelper args={[5]} />
 
         <SelectedProvider>
           <Radar />

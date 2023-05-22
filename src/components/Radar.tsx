a11e32c77349896566b799be74e79b1aff668de8
Kyle Macquarrie <kyle_macquarrie@next.co.uk>
2023-05-22 13:53:30 +0100

    Clear selected item on navigation

--- src/components/Radar.tsx
+++ src/components/Radar.tsx
@@ -1,10 +1,20 @@
 import { items } from '../utils/parse-csv'
 import { Ring } from './Ring'
 import { buildRings } from '../utils/buildRings'
+import { useLocation } from 'react-router-dom'
+import { useContext, useEffect } from 'react'
+import { SelectedContext } from '../SelectedContext'
 
 const rings = buildRings(items)
 
 export function Radar() {
+  const location = useLocation()
+  const { setSelected } = useContext(SelectedContext)
+
+  useEffect(() => {
+    setSelected('')
+  }, [location, setSelected])
+
   return (
     <>
       {rings.map((r, index) => (

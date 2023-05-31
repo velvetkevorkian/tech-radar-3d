6cb5c52a588a5ebabd43be908d64b60fda231be8
Kyle Macquarrie <kyle_macquarrie@next.co.uk>
2023-05-31 15:57:18 +0100

    Refactor how points are distributed on the quadrant

--- src/utils/buildRings.ts
+++ src/utils/buildRings.ts
@@ -3,38 +3,75 @@ import remap from './remap'
 import type { QuadrantName, Item, Ring } from '../types'
 import { rings, quadrants } from '../types'
 
-function randomPositionForQuadrant(quadrant: QuadrantName, radius: number) {
-  let phi = 0 // latitude (north/south)
-  let theta = 0 // longitude (east/west)
+const { PI } = Math
+const HALF_PI = PI / 2
 
+function positionsForQuadrant(count: number, quadrant: QuadrantName) {
+  const rows: number[][] = []
+
+  let row = 0
+  let total = 0
+
+  while (total < count) {
+    // TODO: don't fill last row unless needed?
+    rows[row] = new Array(row + 1).fill(row)
+    row++
+    total += row
+  }
+
+  const rowAngleStep = Math.PI / 2 / (rows.length + 2)
+
+  const positions = rows
+    .map((r, i) => {
+      const phi = rowAngleStep * (i + 1)
+      const columnAngleStep = Math.PI / (r.length + 1)
+      const vectors = r.map((_, j) => {
+        const theta = columnAngleStep * (j + 1)
+        return remapAnglesForQuadrant({ quadrant, phi, theta })
+      })
+      return vectors
+    })
+    .flat()
+
+  return positions
+}
+
+function remapAnglesForQuadrant({
+  quadrant,
+  phi, // latitude (north/south)
+  theta, // longitude (east/west)
+}: {
+  quadrant: QuadrantName
+  phi: number
+  theta: number
+}) {
   switch (quadrant) {
     case 'languages-and-frameworks': {
       // bottom right
-      phi = remap(Math.random(), 0, 1, Math.PI / 2, Math.PI)
-      theta = remap(Math.random(), 0, 1, 0, Math.PI)
+      phi = remap(phi, 0, HALF_PI, HALF_PI, PI)
+      theta = remap(theta, 0, PI, 0, PI)
       break
     }
     case 'tools': {
       // top right
-      phi = remap(Math.random(), 0, 1, 0, Math.PI / 2)
-      theta = remap(Math.random(), 0, 1, 0, Math.PI)
+      phi = remap(phi, 0, HALF_PI, 0, HALF_PI)
+      theta = remap(theta, 0, PI, 0, PI)
       break
     }
     case 'techniques': {
       // top left
-      phi = remap(Math.random(), 0, 1, 0, Math.PI / 2)
-      theta = remap(Math.random(), 0, 1, -Math.PI, 0)
+      phi = remap(phi, 0, HALF_PI, 0, HALF_PI)
+      theta = remap(theta, 0, PI, -PI, 0)
       break
     }
     case 'platforms': {
       // bottom left
-      phi = remap(Math.random(), 0, 1, Math.PI / 2, Math.PI)
-      theta = remap(Math.random(), 0, 1, -Math.PI, 0)
+      phi = remap(phi, 0, HALF_PI, HALF_PI, PI)
+      theta = remap(theta, 0, PI, -PI, 0)
       break
     }
   }
-
-  return new Vector3().setFromSphericalCoords(radius, phi, theta)
+  return { phi, theta }
 }
 
 export function radiusForIndex(index: number) {
@@ -46,23 +83,30 @@ export function buildRings(items: Item[]) {
     return {
       name: r,
       quadrants: quadrants.map((q) => {
+        const itemsForQuadrant = items.filter(
+          (item) =>
+            item.ring.toLowerCase() === r && item.quadrant.toLowerCase() === q
+        )
+        const anglesForQuadrant = positionsForQuadrant(
+          itemsForQuadrant.length,
+          q
+        )
+
         return {
           name: q,
           ring: r,
-          items: items
-            .filter(
-              (item) =>
-                item.ring.toLowerCase() === r &&
-                item.quadrant.toLowerCase() === q
+          items: itemsForQuadrant.map((item, j) => {
+            const { phi, theta } = anglesForQuadrant[j]
+            const position = new Vector3().setFromSphericalCoords(
+              radiusForIndex(i),
+              phi,
+              theta
             )
-            .map((item) => {
-              // TODO: distribute evenly across the sphere's surface
-              const position = randomPositionForQuadrant(q, radiusForIndex(i))
-              return {
-                item,
-                position,
-              }
-            }),
+            return {
+              item,
+              position,
+            }
+          }),
         }
       }),
     } as Ring
